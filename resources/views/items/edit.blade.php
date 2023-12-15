0f97d5b696d09b06b2c3550ea2c55aaee1cb093e
Danny Ruggles <dannyruggles@hotmail.com>
2023-12-15 19:36:19 -0400

    9

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -12,7 +12,7 @@
                 <select name="category_id" id="category_id" class="form-control">
                     <option value="">Select a category</option>
                     @foreach ($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
                 </select>
                 @error('category_id')
